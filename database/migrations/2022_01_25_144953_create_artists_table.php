a4636ce8d0e3c6322b8266ee216bae7eb94a7a8b
cenny1988 <59100992+cenny1988@users.noreply.github.com>
2022-01-25 15:58:39 +0100

    create table. Add music.png schema

---
+++ database/migrations/2022_01_25_144953_create_artists_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArtistsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artists', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 60);
+            $table->string('surname', 60);
+            $table->date('date_of_birth');
+            $table->string('birth_city', 60);
+            $table->string('email', 60)->unique();
+            $table->string('phone', 20)->unique()->nullable();
+            $table->string('web-site', 60)->unique();
+            $table->string('address');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artists');
+    }
+}
---
+++ database/migrations/2022_01_25_145404_create_albums_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlbumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name', 60)->unique();
+            $table->date('year');
+            $table->string('imei', 60)->unique();
+            $table->string('title', 60)->unique();
+            $table->integer('price');
+            $table->string('record_company');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+}
